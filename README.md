a73c3e913dc77abf0b2e1bfcb901f2db72a33e00
Sarah Sukardi <sukardi@jhu.edu>
2016-06-09 00:19:01 -0700

    Fixing alignment issues

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ## About
 I decided my first javascript project would be a little chrome extension dealing with a pesky and (imho) extremely annoying problem (especially when you're horribly impatient and just trying to pull up some music to sightread): the 15-second IMSLP waiting time. So many curses on that!
 
-This extension adds a button which gives a link to download the pdf without having to wait!!! Bypass the 15-second waiting time and save (15 * n)/60 (n = the number of times you download something from imslp) minutes of your life! If you're a classical musician (or just some amateur like me), that's a lot of minutes.
+This extension adds a button which gives a link to download the pdf without having to wait!!! Bypass the 15-second waiting time and save (15 * n)/60 (n = the number of times you download something off imslp) minutes of your life! If you're a classical musician (or just some amateur like me), that's a lot of minutes.
 
 ## Dedication
-I'm not usually one to dedicate apps (lol), but this one's for Nicolo Paganini, whose concertos on IMSLP motivated this extension!
+I'm not usually one to dedicate apps (lol), but this one's for Nicolo Paganini, whose concertos I was trying (miserably) to sightread on IMSLP motivated this extension!
 
 ## Developers
-Just good 'ol me, <a href="github.com/smsukardi>Sarah</a> :)
+Just good 'ol me, <a href="github.com/smsukardi">Sarah</a> :)
---
+++ downloadButton.js
@@ -0,0 +1,52 @@
+function begin() {
+  addDownloadButton();
+}
+
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    //Can assume that "tabs" is nonempty. 
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+}
+
+/** Adds download button to the web page */
+function addDownloadButton() {
+  //Retrieves the link
+  var $getLink = document.querySelector('#sm_dl_wait');  
+  //console.log($getLink);
+
+  //actual link is under data-id tag
+  var link = $getLink.dataset.id;
+
+  //var $getAttachDiv = document.querySelectorAll('div[style="font-size:2em;font-family:Georgia,serif"]');
+  var $getAttachDiv = document.querySelectorAll('center');
+  $getFirst = $getAttachDiv[0];
+
+  //Creates a button to download pdf
+  var $button = document.createElement('button');
+  $button.innerText = 'Download PDF';
+
+  $getFirst.parentNode.insertBefore($button, $getFirst);
+  $getFirst.parentNode.removeChild($getFirst)
+
+  $button.addEventListener('click', function() {window.open(link, '_blank')});
+}
+
+/** Directly goes to the PDF link */
+function directDownload() {
+    var $getLink = document.querySelector('#sm_dl_wait'); 
+    var link = $getLink.dataset.id;
+    window.open(link, '_blank');
+}
+
+begin();
